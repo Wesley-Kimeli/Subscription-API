902d1f3952cd9034995c07c049fa67aca041526b
Wesley Kimeli <wesleykirui2021@gmail.com>
2025-10-03 13:23:28 +0300

    fix

--- config/env.js
+++ config/env.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const { PORT } = process.env;st
+export const { PORT } = process.env;
