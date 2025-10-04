4d724135b253235a20e5400a2017c1084d49d9d6
Wesley Kimeli <wesleykirui2021@gmail.com>
2025-10-04 16:45:10 +0300

    feat: integrate MongoDB with Mongoose and add user/subscription models
    Implement MongoDB connection logic and environment variable support
    Implement Mongoose models for User and Subscription with validation
    Update app entry to connect to database on server start
    Update environment config to include DB_URI
    Add mongoose and mongodb dependencies
    Update package-lock.json for new dependencies

---
+++ database/mongodb.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import { DB_URI, NODE_ENV} from '../config/env.js';
+
+if(!DB_URI) {
+    throw new Error('MongoDB URI is missing, please define MONGODB_URI in .env.<development/production>.local');
+}
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(DB_URI);
+
+        console.log(`Connected to Database in ${NODE_ENV} mode`);
+    }catch(error) {
+        console.error('error connecting to database: ', error);
+
+        process.exit(1);
+    }
+}
+
+export default connectToDatabase;
